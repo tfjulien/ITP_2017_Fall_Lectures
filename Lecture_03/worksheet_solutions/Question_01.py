588a13ea2b2e09c04b0edd84188a81fd2ce92d79
groveh01 <himan.grover@gmail.com>
2017-09-21 14:45:01 -0400

    worksheet_01 solutions.

---
+++ Lecture_03/worksheet_solutions/Question_01.py
@@ -0,0 +1,15 @@
+
+# get input from the user, we assume the input is always going to be a positive integer
+n = int(input("Enter a positive integer: "))
+
+# iterate over the rows, these go from 1 -> n
+for row in range(1, n+1):
+    # clear the previous line
+    line = ''
+    # iterate from 1 to the current row
+    # this will result in the 3rd row having 3 elements
+    for col in range(1, row+1):
+        # add an '*' for each column
+        line += '* '
+    # print this line now that we have the correct number of columns
+    print(line)
---
+++ Lecture_03/worksheet_solutions/Question_03.py
@@ -0,0 +1,31 @@
+n = int(input("Enter a positive integer: "))
+
+# keep track of how many numbers there are
+count = 1
+
+for row in range(1, n+1):
+    line = ''
+    for col in range(1, row+1):
+        # add the current number to the line
+        line += str(count) + ' '
+        # add 1 to the count for the next position
+        count += 1
+    print(line)
+
+# solution with formatting
+# there are many ways to do this
+
+count = 1
+
+# this is the formula to calculate triangle numbers
+greatest_int = n * (n+1) // 2
+# knowing how many characters there are in the largest numbers is required to calculate the offset
+number_of_characters = len(str(greatest_int))
+
+for row in range(1, n+1):
+    line = ''
+    for col in range(1, row+1):
+        # this will offset the numbers to ensure that they are right aligned
+        line += '{:>{}} '.format(count, number_of_characters)
+        count += 1
+    print(line)
