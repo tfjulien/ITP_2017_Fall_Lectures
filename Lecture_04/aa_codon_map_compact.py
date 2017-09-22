2a1e8a36567023b0ba0b9277780490b999c0b750
groveh01 <himan.grover@gmail.com>
2017-09-22 12:20:03 -0400

    updated lecture_04 jupyter notebook and aa_codon_map example.

---
+++ Lecture_04/aa_codon_map_compact.py
@@ -0,0 +1,15 @@
+import pprint
+gen_code = {'uuu': 'Phe', 'uuc': 'Phe', 'uua': 'Leu', 'uug': 'Leu',
+          'ucu': 'Ser', 'ucc': 'Ser', 'uca': 'Ser', 'ucg': 'Ser',
+          'uau': 'Tyr', 'uac': 'Tyr', 'uaa': 'Stop', 'uag': 'Stop',
+          'ugu': 'Cys', 'ugc': 'Cys', 'uga': 'Stop', 'ugg': 'Trp',
+          'cuu': 'Leu', 'cuc': 'Leu', 'cua': 'Leu', 'cug': 'Leu',
+          'ccu': 'Pro', 'ccc': 'Pro', 'cca': 'Pro', 'ccg': 'Pro',
+          'cau': 'His', 'cac': 'His', 'caa': 'Gln', 'cag': 'Gln',
+          'cgu': 'Arg', 'cgc': 'Arg', 'cga': 'Arg', 'cgg': 'Arg'}
+
+aa_codon_map = {}
+for key, value in gen_code.items():
+    aa_codon_map[value] = aa_codon_map.get(value, []) + [key]
+
+pprint.pprint(aa_codon_map)
