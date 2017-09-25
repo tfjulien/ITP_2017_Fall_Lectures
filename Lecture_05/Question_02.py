17803c7d0359dcc297e9d8605fab6d0ec48ade6d
markgrivainis <grvmar006@myuct.ac.za>
2017-09-25 13:14:53 -0400

    Added notebook final files for lecture 5

--- Lecture_05/Question_02.py
+++ Lecture_05/Question_02.py
@@ -1,6 +1,7 @@
-# pseudo code
-# 1.
-# 2.
-# 3.
-#   4.
-# 5.
+print('\ncontinue statement')
+numbers = [1, 3, 5, 6, 9, 10]
+for number in numbers:
+    if number % 2 == 0:
+        break
+    print(number)
+print('finished')
