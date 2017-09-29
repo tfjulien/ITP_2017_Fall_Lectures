4733935ad1f1854ab1f6b7352709d1e97dc4654d
markgrivainis <grvmar006@myuct.ac.za>
2017-09-29 10:29:01 -0400

    Added notebook with answers for worksheet 2 Added lecture 6 - functions

--- Lecture_05/Question_02.py
+++ Lecture_05/Question_02.py
@@ -1,7 +1,28 @@
-print('\ncontinue statement')
-numbers = [1, 3, 5, 6, 9, 10]
-for number in numbers:
-    if number % 2 == 0:
-        break
-    print(number)
-print('finished')
+print(7 % 3)
+# print('\ncontinue statement')
+# numbers = [1, 3, 5, 6, 9, 10]
+# for number in numbers:
+#     if number % 2 == 0:
+#         break
+#     print(number)
+# # print('finished')
+# 1 2 3 4 5 6 7
+# # n 2 n 2 n n n
+# lst = ['1', '2']
+# print(lst)
+# print('-----'.join(lst))
+#
+# print(lst)
+# print('-----'.join(lst))
+# i = 2
+# j = 3
+# m = 1
+# n = 7
+# x | x % 2 == 0  | x % 3 != 0|
+# 1 | False       | True      | False
+# 2 | True        | True      | True
+# 3 | False       | False     | False
+# 4 | True        | True      | True
+# 5 | False       | True      | False
+# 6 | True        | False     | False
+# 7 | False       | True      | False
